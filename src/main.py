39e0c7fdc6ec43d07ef9ed7f457491ac802de050
Dariel Sevilla <darielsevilla@gmail.com>
2025-02-08 16:22:38 -0600

    project created

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import filereader as File
+
+def main():
+    fileReader = File.FileReader("dataset\EjemploEstudiantes.csv")
+    fileReader.readData()
+    fileReader.centerReduce()
+
+if __name__ == '__main__':
+    main()
+    
