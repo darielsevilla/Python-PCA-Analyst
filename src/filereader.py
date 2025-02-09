5d54acf3e9933232f136adb554009fd17335e91b
Dariel Sevilla <darielsevilla@gmail.com>
2025-02-08 19:54:41 -0600

    correlation matrix

--- src/filereader.py
+++ src/filereader.py
@@ -27,24 +27,76 @@ class FileReader:
                 #convertir cada elemento de esa columna en numeros
                 for i in range(len(columnValues)):
                     columnValues[i] = float(columnValues[i].replace(",","."))
-                
+
                 #media de columna
                 mean = np.mean(columnValues)
-                deviation = np.sqrt(2)
+                numerator = 0
+                #desviacion estandar
+                for j in range(len(columnValues)):
+                    numerator += ((columnValues[j]-mean)*(columnValues[j]-mean))
 
-                #centrar y reducir elementos
-        
-                for item in columnValues:
-                    changedColumn.append(float((item-mean)/deviation))
+                deviation = np.sqrt(numerator/len(columnValues))
                 
+                #centrar y reducir elementos
+                for item in columnValues:
+                    print(item, " ")
+                    changedColumn.append(float((item-mean)/deviation))
+                print("\n")
                 #cambiar por datos actualizados
-                #print("changing column ",column, " fin")
                 dataReduced[columnName] = changedColumn
                 
-        self.dataReduced = dataReduced 
-        print(dataReduced)       
+                
+        self.dataProcessed = dataReduced 
+        #print(dataReduced)       
+    
+    def printMatrix(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                print("[",matrix[i][j],"]",end="")
+            print("")
+
+    def createRelationsMatrix(self):
+        #para pasos 1
+        self.centerReduce()
+
+        #empezando el paso 2
+        #consiguiendo matriz transpuesta
+        rows = self.dataProcessed.values
+        print(rows)
+        columns = self.dataProcessed.columns
+
+        transposed = np.empty((len(columns), len(rows)))  
+        
+        for i, row in enumerate(rows):
+            for j in range(len(columns)):
+                if(isinstance(row[j],float)):
+                    transposed[j][i] = row[j]
+                else:
+                    transposed[j][i] = None
+           
+
+        #numero escalar que hay que multiplicar
+        
+        scalar = 1/(len(rows))
+        print(self.dataProcessed)
+
+        
+        #consiguiendo la matriz de correlaciones
+        correlationMatrix = []
+        for i in range(1,len(transposed)):
+            row = []
+            for j in range(1,len(columns)):
+                #multiplicacion matricial
+                item = 0
+                for k in range(len(transposed[0])):
+                    item += (transposed[i][k] * rows[k][j]*scalar)   
+                row.append(item)
+            correlationMatrix.append(row)
+        
+        self.correlationMatrix = correlationMatrix                
+        return correlationMatrix
+    
 
 
     
-
         
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import filereader as File
 def main():
     fileReader = File.FileReader("dataset\EjemploEstudiantes.csv")
     fileReader.readData()
-    fileReader.centerReduce()
+    fileReader.createRelationsMatrix()
 
 if __name__ == '__main__':
     main()
